7894d9eee7daf227f32288a46983796464f44a68
Amy Haerr <amyhaerr@MacBook-Pro.local>
2020-06-22 15:26:10 -0500

    work on card sizing

--- clicky-game/src/components/Card/Card.js
+++ clicky-game/src/components/Card/Card.js
@@ -2,15 +2,12 @@ import React from "react";
 import "./Card.css";
 
 const Cards = props => (
-    <div className="card" 
+    <div 
+    className="card" 
     value={props.id}
     onClick={() => props.handleClick(props.id)}>
         <div className="img-container">
-            <img alt={props.title} src={props.image}/>
-            <div className="overlay">
-                <div className="text">
-                </div>
-            </div>
+            <img alt={props.name} src={props.image}/>  
         </div>
     </div>
 
