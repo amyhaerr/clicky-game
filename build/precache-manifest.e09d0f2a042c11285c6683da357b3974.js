e9dbc829755f8792402aedef51631bb2d4060617
Amy Haerr <amyhaerr@MacBook-Pro.local>
2020-06-22 19:46:39 -0500

    add build folder

---
+++ build/precache-manifest.e09d0f2a042c11285c6683da357b3974.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ed3cbdbd51fc05c1a93b1bbe0837ca1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3373ff82ace9bc3953f6",
+    "url": "/static/css/main.09e83bb2.chunk.css"
+  },
+  {
+    "revision": "77ec5a0aa6834a00ede5",
+    "url": "/static/js/2.69b5d891.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.69b5d891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3373ff82ace9bc3953f6",
+    "url": "/static/js/main.cd0fbd97.chunk.js"
+  },
+  {
+    "revision": "22eabea025eba006b39b",
+    "url": "/static/js/runtime-main.34b1f622.js"
+  }
+]);
