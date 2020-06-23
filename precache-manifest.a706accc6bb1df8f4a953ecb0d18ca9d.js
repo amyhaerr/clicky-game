c43880d7ee0b3229c34bb80d40bfd59a23640c74
Amy Haerr <amyhaerr@MacBook-Pro.local>
2020-06-22 20:18:13 -0500

    Updates

---
+++ precache-manifest.a706accc6bb1df8f4a953ecb0d18ca9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d250cecd5d3bf32a9b8bd835d4d5986a",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "c4a8a8f52b3a35ef697e",
+    "url": "/clicky-game/static/css/main.09e83bb2.chunk.css"
+  },
+  {
+    "revision": "77ec5a0aa6834a00ede5",
+    "url": "/clicky-game/static/js/2.69b5d891.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.69b5d891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4a8a8f52b3a35ef697e",
+    "url": "/clicky-game/static/js/main.014d8be7.chunk.js"
+  },
+  {
+    "revision": "a2dd66fdceb11b57b463",
+    "url": "/clicky-game/static/js/runtime-main.3c5f3143.js"
+  }
+]);
